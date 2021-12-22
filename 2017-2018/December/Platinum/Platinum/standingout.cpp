9b8ecaec3df7bd0c9452bd05d965deeea3ef57bf
hruif <62226693+hruif@users.noreply.github.com>
2021-12-21 19:35:30 -0800

    contest

---
+++ 2017-2018/December/Platinum/Platinum/standingout.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <fstream>
+#include <vector>
+
+using namespace std;
+
+ifstream fin("standingout.in");
+ofstream fout("standingout.out");
+
+int N;
+
+int main() {
+	fin >> N;
+
+}
